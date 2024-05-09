591d3826962c2fcf7e3c278977294fcfe71d375d
ZubbsZubbs <thankgodazubuike194@gmail.com>
2024-05-09 18:04:02 +0100

    commit

---
+++ core/management/commands/create_superuser.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Creates a default superuser'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('budingtech', 'thankgodazubuike@gmail.com', '1789')
+            self.stdout.write(self.style.SUCCESS('Successfully created a new superuser'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists.'))
