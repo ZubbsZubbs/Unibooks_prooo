f6a1a3060085cf96ce18683d2d8adeb29bcffd09
ZubbsZubbs <thankgodazubuike194@gmail.com>
2024-05-10 01:21:27 +0100

    commit

--- core/urls.py
+++ core/urls.py
@@ -17,21 +17,15 @@ Including another URLconf
 # core/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('book.urls')),  # Include the book app's URLs at the root
-   
-    
 ]
 
-urlpatterns +=staticfiles_urlpatterns()
-
-
-
-
-
-
+# During development, use Django to serve static and media files
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
